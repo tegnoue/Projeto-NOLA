ab545161a039d0c9497c3d7410fb03b53b5e53cb
tegnoue <tegnoue@outlook.com>
2025-11-03 10:26:18 -0300

    feat(db): implementa option group

---
+++ backend/analytics/model/cubes/option_groups.js
@@ -0,0 +1,56 @@
+cube(`option_groups`, {
+  sql_table: `public.option_groups`,
+  
+  data_source: `default`,
+  
+  joins: {
+    brands: {
+      sql: `${CUBE}.brand_id = ${brands.id}`,
+      relationship: `many_to_one`
+    },
+    
+    categories: {
+      sql: `${CUBE}.category_id = ${categories.id}`,
+      relationship: `many_to_one`
+    },
+    
+    sub_brands: {
+      sql: `${CUBE}.sub_brand_id = ${sub_brands.id}`,
+      relationship: `many_to_one`
+    }
+  },
+  
+  dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primary_key: true
+    },
+    
+    name: {
+      sql: `name`,
+      type: `string`
+    },
+    
+    pos_uuid: {
+      sql: `pos_uuid`,
+      type: `string`
+    },
+    
+    deleted_at: {
+      sql: `deleted_at`,
+      type: `time`
+    }
+  },
+  
+  measures: {
+    count: {
+      type: `count`
+    }
+  },
+  
+  pre_aggregations: {
+    // Pre-aggregation definitions go here.
+    // Learn more in the documentation: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  }
+});
